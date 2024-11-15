2490d72b1acbe24fa926800d9c48a71978bf6ffa
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2024-11-15 17:47:52 +0900

    Fixed Footsteps Bug

--- Assets/Main/Player/FootstepsPlayer.cs
+++ Assets/Main/Player/FootstepsPlayer.cs
@@ -9,8 +9,7 @@ public class FootstepsPlayer : MonoBehaviour
     [SerializeField]
     private float _interval = 0.5f;
 
-    private Coroutine _playCoroutine;
-    private bool _isPlaying;
+    private float _intervalTimer;
 
     private AudioClip FootstepSound => _footstepSounds[UnityEngine.Random.Range(0, _footstepSounds.Length)];
 
@@ -19,6 +18,7 @@ public class FootstepsPlayer : MonoBehaviour
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _intervalTimer = _interval;
     }
 
     private void Update()
@@ -28,42 +28,20 @@ public class FootstepsPlayer : MonoBehaviour
             return;
         }
 
-        if (_rigidbody.velocity.magnitude > 0.1f && !_isPlaying)
+        if (_intervalTimer > 0)
+        {
+            _intervalTimer -= Time.deltaTime;
+        }
+
+        if (_rigidbody.velocity.magnitude > 0.1f && _intervalTimer <= 0f)
         {
             Play();
-        }
-        else if (_rigidbody.velocity.magnitude <= 0.1f && _isPlaying)
-        {
-            Stop();
+            _intervalTimer = _interval;
         }
     }
 
     public void Play()
     {
-        Stop();
-        if (_playCoroutine == null)
-        {
-            _isPlaying = true;
-            _playCoroutine = StartCoroutine(PlayFootsteps());
-        }
-    }
-
-    public void Stop()
-    {
-        if (_playCoroutine != null)
-        {
-            _isPlaying = false;
-            StopCoroutine(_playCoroutine);
-            _playCoroutine = null;
-        }
-    }
-
-    private IEnumerator PlayFootsteps()
-    {
-        while (true)
-        {
-            AudioSource.PlayClipAtPoint(FootstepSound, transform.position, Config.AudioVolume);
-            yield return new WaitForSeconds(_interval);
-        }
+        AudioSource.PlayClipAtPoint(FootstepSound, transform.position, Config.AudioVolume);
     }
 }
